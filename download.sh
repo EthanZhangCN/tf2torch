cbd40addf8162b0cb3414d29541af53e6c18bdcb
EthanZhang <ermzdy@163.com>
2022-06-28 15:57:44 +0800

    add parameters download shell

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget https://download.pytorch.org/models/vgg16-397923af.pth
+wget https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5
